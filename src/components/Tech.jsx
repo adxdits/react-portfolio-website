f05eef60271d74c33b32b5fd87633082c7349abb
ALMOUNAYAR Adam <adamalmounayar@hotmail.com>
2024-04-07 00:58:23 +0200

    new-responsibity-fixed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,15 +3,11 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-
+const icons = Object.values(technologies).map((technology) => technology.icon);
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+    <div className="w-full h-[75vh] mx-auto">
+      <BallCanvas icons={icons} />
     </div>
   );
 };
